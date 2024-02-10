ae2e3eaf4e7272736e96cb63c91c786086316cfc
luk3mn <lucasrenan2015@gmail.com>
2024-02-10 11:57:52 -0300

    feat: working on orm: jpa to entity mapping in entities students, certifications and answers

--- src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
@@ -1,8 +1,17 @@
 package com.rocketseat.certification_nlw.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +19,20 @@ import lombok.NoArgsConstructor;
 @Data // Getter and Setters automatically
 @AllArgsConstructor // create a constructor with all atributes
 @NoArgsConstructor // empty constructor
+@Entity(name = "students") // ORM -> transform in table in database
 public class StudentEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false) // On database: must be unique and not acept null value
     private String email;
-    private List<CertificationStudentEntity> certificationStudentEntity;
     
+    @OneToMany(mappedBy = "studentEntity") // one student may have many certifications
+    private List<CertificationStudentEntity> certificationStudentEntity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
